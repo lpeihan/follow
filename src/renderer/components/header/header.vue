bbc05aa82bd34b281bd0558a45be93308baa36f5
李培汉 <lipeihan@chandashi.com>
2019-02-26 17:04:22 +0800

    style: 修改样式

--- src/renderer/components/header/header.vue
+++ src/renderer/components/header/header.vue
@@ -1,16 +1,20 @@
 <template>
   <header class="header" @dblclick="handleDbclick">
-    <div class="header-icons">
-      <icon name="music" @click="$router.push('/')"></icon>
-      <icon name="refresh" @click="refresh"></icon>
-      <icon name="back" @click="$router.go(-1)"></icon>
-      <icon name="right" @click="$router.go(1)"></icon>
+    <div class="header-bar"></div>
+
+    <div class="header-content">
+      <div class="icons">
+        <icon name="music" @click="$router.push('/')"></icon>
+        <icon name="refresh" @click="refresh"></icon>
+        <icon name="back" @click="$router.go(-1)"></icon>
+        <icon name="right" @click="$router.go(1)"></icon>
+      </div>
+      <ul class="navbars">
+        <li class="navbar" v-for="navbar in navbars" :key="navbar.label">
+          <router-link class="navbar-link" :to="navbar.to">{{navbar.label}}</router-link>
+        </li>
+      </ul>
     </div>
-    <ul class="header-navbars">
-      <li class="navbar" v-for="navbar in navbars" :key="navbar.label">
-        <router-link class="navbar-link" :to="navbar.to">{{navbar.label}}</router-link>
-      </li>
-    </ul>
   </header>
 </template>
 
@@ -25,7 +29,7 @@ export default {
         { label: '排行榜', to: '/ranking' },
         { label: '搜索', to: '/search' },
         { label: '我的', to: '/my' },
-        { label: '关于', to: 'about' }
+        { label: '关于', to: '/about' }
       ]
     };
   },
@@ -44,50 +48,56 @@ export default {
 .header
   fixed: top left 50px right 50px
   z-index: 100
-  display: flex
-  padding-top: 20px
-  align-items: center
   height: 80px
   background: linear-gradient(90deg, $color-bg, $color-sub-bg)
-  -webkit-app-region: drag
   user-select: none
 
-  &-icons
-    flex: 1
+  &-bar
+    height: 30px
+    -webkit-app-region: drag
+
+  &-content
+    height: 50px
     display: flex
     align-items: center
 
-    .icon
-      cursor: pointer
-      size: 16px
-      color: $color-text-l
-      &:hover
+    .icons
+      flex: 1
+      display: flex
+      align-items: center
+
+      .icon
+        cursor: pointer
+        size: 16px
+        color: $color-text-l
+
+        &:hover
+          color: $color-theme
+
+      .icon-music
+        size: 32px
         color: $color-theme
-    
-    .icon-music
-      size: 32px
-      color: $color-theme
-      margin-right: 20px
-    
-    .icon-refresh
-      size: 22px
-      margin-right: 20px
-    
-    .icon-back
-      margin-right: 5px
+        margin-right: 20px
 
-  &-navbars
-    color: $color-text-l
-    font-size: $font-size-large
+      .icon-refresh
+        size: 22px
+        margin-right: 20px
 
-    .navbar
-      float: left
-      margin-right: 15px
+      .icon-back
+        margin-right: 5px
 
-      &:last-child
-        margin-right: 0
-        font-size: 15px
-      
-    .navbar-link
-      transition: color $transition-time
+    .navbars
+      color: $color-text-l
+      font-size: $font-size-large
+
+      .navbar
+        float: left
+        margin-right: 15px
+
+        &:last-child
+          margin-right: 0
+          font-size: 15px
+
+        .navbar-link
+          transition: color $transition-time
 </style>
